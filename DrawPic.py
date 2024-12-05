f2fbf6967d33b085b819b2c380d9359998531055
youngYangtze <116358132+youngYangtze@users.noreply.github.com>
2024-12-05 21:45:17 +0800

    feat: Set save and show in self and XYZ in function

--- DrawPic.py
+++ DrawPic.py
@@ -1,11 +1,12 @@
 # @Time    : 12/5/2024
 # @Author  : wzyang
 # @File    : DrawPic.py
+import os
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import seaborn as sns
+import pandas as pd
 import numpy as np
-import os
 
 
 class DrawPic:
@@ -19,6 +20,8 @@ class DrawPic:
         figsize=(16, 9),
         use_seaborn=True,
         seaborn_style="whitegrid",
+        default_save=True,
+        default_show=True,
     ):
         self.xlabel = xlabel
         self.ylabel = ylabel
@@ -27,6 +30,8 @@ class DrawPic:
         self.default_colors = ["#8891DB", "#C7988C", "#A5C496"]
         self.use_seaborn = use_seaborn
         self.seaborn_style = seaborn_style
+        self.default_save = default_save
+        self.default_show = default_show
         if use_seaborn:
             self.set_seaborn_style()  # 设置Seaborn风格
 
@@ -43,14 +48,21 @@ class DrawPic:
     def plot_line(
         self,
         *args,
+        xlabel=None,
+        ylabel=None,
         save_path=None,
         figsize=None,
         equal_aspect=False,
         colors=None,
-        show=True
+        save=None,
+        show=None
     ):
         if figsize is None:
             figsize = self.default_figsize
+        if save is None:
+            save = self.default_save
+        if show is None:
+            show = self.default_show
 
         fig, ax = plt.subplots(figsize=figsize)
         if colors is None:
@@ -59,13 +71,13 @@ class DrawPic:
             x, y, label = args[i], args[i + 1], args[i + 2]
             color = colors[i // 3 % len(colors)]
             ax.plot(x, y, label=label, color=color)
-        ax.set_xlabel(self.xlabel)
-        ax.set_ylabel(self.ylabel)
+        ax.set_xlabel(xlabel if xlabel else self.xlabel)
+        ax.set_ylabel(ylabel if ylabel else self.ylabel)
         ax.legend()
         ax.grid(True)
         if equal_aspect:
             ax.set_aspect("equal", adjustable="box")
-        if save_path:
+        if save and save_path:
             plt.savefig(save_path, dpi=300)
         if show:
             plt.show()
@@ -73,14 +85,22 @@ class DrawPic:
     def plot_bar(
         self,
         *args,
+        xlabel=None,
+        ylabel=None,
         save_path=None,
         figsize=None,
         equal_aspect=False,
         colors=None,
-        show=True
+        save=None,
+        show=None
     ):
         if figsize is None:
             figsize = self.default_figsize
+        if save is None:
+            save = self.default_save
+        if show is None:
+            show = self.default_show
+
         fig, ax = plt.subplots(figsize=figsize)
         if colors is None:
             colors = self.default_colors
@@ -91,15 +111,15 @@ class DrawPic:
             x, y, label = args[i], args[i + 1], args[i + 2]
             color = colors[i // 3 % len(colors)]
             ax.bar(indices + i * bar_width, y, bar_width, label=label, color=color)
-        ax.set_xlabel(self.xlabel)
-        ax.set_ylabel(self.ylabel)
+        ax.set_xlabel(xlabel if xlabel else self.xlabel)
+        ax.set_ylabel(ylabel if ylabel else self.ylabel)
         ax.set_xticks(indices + bar_width * (n_bars - 1) / 2)
         ax.set_xticklabels(args[0])
         ax.legend()
         ax.grid(True)
         if equal_aspect:
             ax.set_aspect("equal", adjustable="box")
-        if save_path:
+        if save and save_path:
             plt.savefig(save_path, dpi=300)
         if show:
             plt.show()
@@ -107,28 +127,37 @@ class DrawPic:
     def plot_scatter(
         self,
         *args,
+        xlabel=None,
+        ylabel=None,
         save_path=None,
         figsize=None,
         equal_aspect=False,
         colors=None,
-        show=True
+        save=None,
+        show=None,
+        s=1.0
     ):
         if figsize is None:
             figsize = self.default_figsize
+        if save is None:
+            save = self.default_save
+        if show is None:
+            show = self.default_show
+
         fig, ax = plt.subplots(figsize=figsize)
         if colors is None:
             colors = self.default_colors
         for i in range(0, len(args), 3):
             x, y, label = args[i], args[i + 1], args[i + 2]
             color = colors[i // 3 % len(colors)]
-            ax.scatter(x, y, label=label, color=color)
-        ax.set_xlabel(self.xlabel)
-        ax.set_ylabel(self.ylabel)
+            ax.scatter(x, y, label=label, color=color, s=s)
+        ax.set_xlabel(xlabel if xlabel else self.xlabel)
+        ax.set_ylabel(ylabel if ylabel else self.ylabel)
         ax.legend()
         ax.grid(True)
         if equal_aspect:
             ax.set_aspect("equal", adjustable="box")
-        if save_path:
+        if save and save_path:
             plt.savefig(save_path, dpi=300)
         if show:
             plt.show()
@@ -136,14 +165,22 @@ class DrawPic:
     def plot_3d_line(
         self,
         *args,
+        xlabel=None,
+        ylabel=None,
         save_path=None,
         figsize=None,
         equal_aspect=False,
         colors=None,
-        show=True
+        save=None,
+        show=None
     ):
         if figsize is None:
             figsize = self.default_figsize
+        if save is None:
+            save = self.default_save
+        if show is None:
+            show = self.default_show
+
         fig = plt.figure(figsize=figsize)
         ax = fig.add_subplot(111, projection="3d")
         if colors is None:
@@ -152,13 +189,13 @@ class DrawPic:
             x, y, z, label = args[i], args[i + 1], args[i + 2], args[i + 3]
             color = colors[i // 4 % len(colors)]
             ax.plot(x, y, z, label=label, color=color)
-        ax.set_xlabel(self.xlabel)
-        ax.set_ylabel(self.ylabel)
+        ax.set_xlabel(xlabel if xlabel else self.xlabel)
+        ax.set_ylabel(ylabel if ylabel else self.ylabel)
         ax.set_zlabel(self.zlabel)
         ax.legend()
         if equal_aspect:
             ax.set_box_aspect([1, 1, 1])  # aspect ratio is 1:1:1
-        if save_path:
+        if save and save_path:
             plt.savefig(save_path, dpi=300)
         if show:
             plt.show()
@@ -166,14 +203,22 @@ class DrawPic:
     def plot_3d_scatter(
         self,
         *args,
+        xlabel=None,
+        ylabel=None,
         save_path=None,
         figsize=None,
         equal_aspect=False,
         colors=None,
-        show=True
+        save=None,
+        show=None
     ):
         if figsize is None:
             figsize = self.default_figsize
+        if save is None:
+            save = self.default_save
+        if show is None:
+            show = self.default_show
+
         fig = plt.figure(figsize=figsize)
         ax = fig.add_subplot(111, projection="3d")
         if colors is None:
@@ -182,13 +227,13 @@ class DrawPic:
             x, y, z, label = args[i], args[i + 1], args[i + 2], args[i + 3]
             color = colors[i // 4 % len(colors)]
             ax.scatter(x, y, z, label=label, color=color)
-        ax.set_xlabel(self.xlabel)
-        ax.set_ylabel(self.ylabel)
+        ax.set_xlabel(xlabel if xlabel else self.xlabel)
+        ax.set_ylabel(ylabel if ylabel else self.ylabel)
         ax.set_zlabel(self.zlabel)
         ax.legend()
         if equal_aspect:
             ax.set_box_aspect([1, 1, 1])  # aspect ratio is 1:1:1
-        if save_path:
+        if save and save_path:
             plt.savefig(save_path, dpi=300)
         if show:
             plt.show()
@@ -196,14 +241,22 @@ class DrawPic:
     def plot_3d_bar(
         self,
         *args,
+        xlabel=None,
+        ylabel=None,
         save_path=None,
         figsize=None,
         equal_aspect=False,
         colors=None,
-        show=True
+        save=None,
+        show=None
     ):
         if figsize is None:
             figsize = self.default_figsize
+        if save is None:
+            save = self.default_save
+        if show is None:
+            show = self.default_show
+
         fig = plt.figure(figsize=figsize)
         ax = fig.add_subplot(111, projection="3d")
         if colors is None:
@@ -225,13 +278,21 @@ class DrawPic:
         ax.legend()
         if equal_aspect:
             ax.set_box_aspect([1, 1, 1])  # aspect ratio is 1:1:1
-        if save_path:
+        if save_path and save:
             plt.savefig(save_path, dpi=300)
         if show:
             plt.show()
 
     def plot_scatter_colored(
-        self, *args, save_path=None, figsize=None, equal_aspect=False, show=True
+        self,
+        *args,
+        xlabel=None,
+        ylabel=None,
+        save_path=None,
+        figsize=None,
+        equal_aspect=False,
+        save=None,
+        show=None
     ):
         if figsize is None:
             figsize = self.default_figsize
@@ -240,14 +301,14 @@ class DrawPic:
         for i in range(0, len(args), 4):
             x, y, z, label = args[i], args[i + 1], args[i + 2], args[i + 3]
             sc = ax.scatter(x, y, c=z, cmap="viridis", label=label)
-            ax.set_xlabel(self.xlabel)
-            ax.set_ylabel(self.ylabel)
-            ax.legend()
-            cbar = plt.colorbar(sc, ax=ax)
-            cbar.set_label(self.zlabel)
+        ax.set_xlabel(xlabel if xlabel else self.xlabel)
+        ax.set_ylabel(ylabel if ylabel else self.ylabel)
+        ax.legend()
+        cbar = plt.colorbar(sc, ax=ax)
+        cbar.set_label(self.zlabel)
         if equal_aspect:
             ax.set_aspect("equal", adjustable="box")
-        if save_path:
+        if save_path and save:
             plt.savefig(save_path, dpi=300)
         if show:
             plt.show()
